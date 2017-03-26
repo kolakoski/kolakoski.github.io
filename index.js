e5abcdd036ad54db09890f1aaafe934eea64d008
michael-chong <michael.yc.chong@gmail.com>
2017-03-25 23:36:35 -0400

    March 25 Edits

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var K2 = "1 22"
 var i = 3 //block number
 var N1 = 1 //number of 1s
 var N = 3 //number of total digits
-var speed = 500 //delay in ms when animating
+var speed = 1000 //delay in ms when animating
+var flag = false;
+
 
 google.charts.load('current', {'packages':['corechart']});
 
@@ -14,7 +16,7 @@ var chartOptions = {
   hAxis: {title: 'Sequence Length'},
   animation:{
         duration: speed-20,
-        easing: 'out'
+        easing: 'inandout'
       }
 };
 var chart;
@@ -25,7 +27,7 @@ var freq = [
   [2, 1/2],
   [3,1/3]
 ]
-
+window.document.getElement
 /*
 window.onresize = function(){
   prime();
@@ -40,8 +42,8 @@ function Generate10(){
 }
 
 function prime(){
-  var h = (document.getElementById('frequency_div').clientHeight + 290).toString() + "px";
-  document.getElementById('sequence').style.height = h;
+  // var h = (document.getElementById('frequency_div').clientHeight + 290).toString() + "px";
+  // document.getElementById('construction').style.height = h;
   document.getElementById('title1').innerHTML = "<b>Kolakoski Sequence to " + N.toString() + " digits</b>"
   document.getElementById('sequence').innerText = K2 //print the sequence
   document.getElementById('instances').innerHTML = N1.toString() + "  <small>   of " + N.toString() + " digits</small>" //print the cumulative number of 1s
@@ -50,44 +52,231 @@ function prime(){
   var chartData = new google.visualization.arrayToDataTable(freq, false);
 
   chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-  chart.draw(chartData, chartOptions);
 
+  chart.draw(chartData, chartOptions);
 }
+
 
 function drawChart() {
   if (K1[i-1] == 1 && i%2 == 0){ //if K_i = 1 and i is even
+
     K1 = K1 + "2"
     K2 = K2 + " 2"
     N = N + 1
     freq.push([N,N1/N]);
 
+    table = document.getElementById('construction3');
+    table.innerHTML = ""
+    var row = table.insertRow(0);
+    row.className = "info";
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = "<b>" + (i).toString() + "</b>";
+    cell2.innerHTML = N.toString();
+    cell3.innerHTML = 2;
+
+    table = document.getElementById('construction2');
+    var row = table.insertRow(N-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = (i).toString();
+    cell2.innerHTML = N.toString();
+    cell3.innerHTML = 2;
+
+    table = document.getElementById('construction1');
+    table.deleteRow(1);
+    var row = table.insertRow(1)
+    row.className = "danger"
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    //cell1.innerHTML = (i).toString();
+    cell2.innerHTML = "<b>" + i.toString() + "</b>";
+    cell3.innerHTML = K1[i-1];
+
+    document.getElementById('construction2div').scrollTop = document.getElementById('construction2div').scrollHeight;
+
+
   }
-  else if (K1[i-1] == 2 && i%2 == 0) { //if K_i = 1 and i is odd
+
+  else if (K1[i-1] == 2 && i%2 == 0) { //if K_i = 2 and i is even
+
     K2 = K2 + " 22"
     K1 = K1 + "22"
     N = N + 1
+    table = document.getElementById('construction3');
+    table.innerHTML = ""
+    var row = table.insertRow(0);
+    row.className = "info";
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = "<b>" + (i).toString() + "</b>";
+    cell2.innerHTML = N.toString();
+    cell3.innerHTML = 2;
+
+    table = document.getElementById('construction2');
+    var row = table.insertRow(N-1)
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = (i).toString();
+    cell2.innerHTML = N.toString();
+    cell3.innerHTML = 2;
+
+
+
     freq.push([N,N1/N]);
     N = N + 1
+    table = document.getElementById('construction3')
+    var row = table.insertRow(1);
+    row.className = "info";
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    //cell1.innerHTML = (i).toString();
+    cell2.innerHTML = N.toString();
+    cell3.innerHTML = 2;
     freq.push([N,N1/N]);
+
+    table = document.getElementById('construction2');
+
+    var row = table.insertRow(N-1);
+
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    //cell1.innerHTML = (i).toString();
+    cell2.innerHTML = N.toString();
+    cell3.innerHTML = 2;
+
+    table = document.getElementById('construction1');
+    table.deleteRow(1);
+    var row = table.insertRow(1)
+    row.className = "danger"
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    //cell1.innerHTML = (i).toString();
+    cell2.innerHTML = "<b>" + i.toString() + "</b>";
+    cell3.innerHTML = K1[i-1];
+
+    document.getElementById('construction2div').scrollTop = document.getElementById('construction2div').scrollHeight;
+
   }
   else if (K1[i-1] == 1 && i%2 == 1) {
+
     K1 = K1 + "1"
     K2 = K2 + " 1"
     N1 = N1 + 1
     N = N + 1
     freq.push([N,N1/N]);
+
+    table = document.getElementById('construction3')
+    table.innerHTML = ""
+    var row = table.insertRow(0);
+    row.className = "info";
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = "<b>" + (i).toString() + "</b>";
+    cell2.innerHTML = N.toString();
+    cell3.innerHTML = 1;
+
+    table = document.getElementById('construction2');
+    var row = table.insertRow(N-1);
+    row.className = "active";
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = (i).toString();
+    cell2.innerHTML = N.toString();
+    cell3.innerHTML = 1;
+
+    table = document.getElementById('construction1');
+    table.deleteRow(1);
+
+    var row = table.insertRow(1)
+    row.className = "danger"
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    //cell1.innerHTML = (i).toString();
+    cell2.innerHTML = "<b>" + i.toString() + "</b>";
+    cell3.innerHTML = K1[i-1];
+    document.getElementById('construction2div').scrollTop = document.getElementById('construction2div').scrollHeight;
+
   }
 
   else if (K1[i-1] == 2 && i%2 == 1){
+
+
     K1 = K1 + "11"
     K2 = K2 + " 11"
     N1 = N1 + 1
     N = N + 1
+    table = document.getElementById('construction3')
+    table.innerHTML = ""
+    var row = table.insertRow(0);
+    row.className = "info";
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = "<b>" + (i).toString() + "</b>";
+    cell2.innerHTML = N.toString();
+    cell3.innerHTML = 1;
+
+    table = document.getElementById('construction2');
+    var row = table.insertRow(N-1);
+    row.className = "active";
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = (i).toString();
+    cell2.innerHTML = N.toString();
+    cell3.innerHTML = 1;
+
     freq.push([N,N1/N]);
     N1 = N1 + 1
     N = N + 1
     freq.push([N,N1/N]);
+
+    table = document.getElementById('construction3')
+    var row = table.insertRow(1);
+    row.className = "info";
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    //cell1.innerHTML = (i).toString();
+    cell2.innerHTML = N.toString();
+    cell3.innerHTML = 1;
+
+    table = document.getElementById('construction2');
+    var row = table.insertRow(N-1);
+    row.className = "active";
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    //cell1.innerHTML = (i).toString();
+    cell2.innerHTML = N.toString();
+    cell3.innerHTML = 1;
+
+    table = document.getElementById('construction1');
+    table.deleteRow(1);
+    var row = table.insertRow(1)
+    row.className = "danger"
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    //cell1.innerHTML = (i).toString();
+    cell2.innerHTML = "<b>" + i.toString() + "</b>";
+    cell3.innerHTML = K1[i-1];
+    document.getElementById('construction2div').scrollTop = document.getElementById('construction2div').scrollHeight;
+
   }
+
 
   i = i + 1
 
@@ -96,10 +285,41 @@ function drawChart() {
   document.getElementById('instances').innerHTML = N1.toString() + "  <small>   of " + N.toString() + " digits</small>" //print the cumulative number of 1s
   document.getElementById('frequency1').innerText = (N1/N).toFixed(3) //print proportion of 1s to 2 decimal places
 
+
+
   var chartData = new google.visualization.arrayToDataTable(freq, false);
+
   document.getElementById('generatebutton').disabled = true; // disable button
   document.getElementById('generate10button').disabled = true; // disable button
+
+
   chart.draw(chartData, chartOptions);
+
+  if (flag == false) {
   setTimeout(function(){document.getElementById('generatebutton').disabled = false}, speed); //artificial delay in re-enabling button
   setTimeout(function(){document.getElementById('generate10button').disabled = false}, speed)
+  }
+
+}
+
+function con(){
+  if(flag){
+    drawChart();
+    setTimeout(con, 1000);
+  }
+}
+
+function cont(){
+  flag = true;
+  document.getElementById('contbutton').setAttribute('onClick', 'stop();');
+  document.getElementById('contbutton').value = "Stop"
+  con();
+}
+
+function stop(){
+  flag = false;
+  document.getElementById('contbutton').setAttribute('onClick', 'cont();');
+  document.getElementById('contbutton').value = "I'm a lazy shit"
+  document.getElementById('generatebutton').disabled = false
+  document.getElementById('generate10button').disabled = false
 }
